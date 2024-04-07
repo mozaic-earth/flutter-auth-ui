d59de2aff71574563de24f6f9361b31be2671706
Neil Cuthbert <neilcuthbert@Neils-MacBook-Pro-2.local>
2024-04-07 10:01:23 +0100

    fix pwd reset option flag

--- lib/src/components/supa_email_auth.dart
+++ lib/src/components/supa_email_auth.dart
@@ -253,7 +253,7 @@ class _SupaEmailAuthState extends State<SupaEmailAuth> {
                 },
               ),
               spacer(16),
-              if (widget.passwordResetEnabled && _isSigningIn) ...[
+              if (_isSigningIn) ...[
                 TextButton(
                   onPressed: () {
                     setState(() {
@@ -302,14 +302,16 @@ class _SupaEmailAuthState extends State<SupaEmailAuth> {
                 child: Text(localization.sendPasswordReset),
               ),
               spacer(16),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _forgotPassword = false;
-                  });
-                },
-                child: Text(localization.backToSignIn),
-              ),
+              if (widget.passwordResetEnabled) ...[
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _forgotPassword = false;
+                    });
+                  },
+                  child: Text(localization.backToSignIn),
+                )
+              ],
             ],
             spacer(16),
           ],
